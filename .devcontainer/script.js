891bfe761305eae23714daf4143d250c51362b7b
JTeo99 <>
2023-09-05 11:29:42 +0000

    CSS added for core components - missing css for timer and difficulty buttons

---
+++ .devcontainer/script.js
@@ -0,0 +1,58 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    background-color: #fff;
+    max-width: 600px;
+    margin: 50px auto;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+h1 {
+    color: #333;
+}
+
+#quote {
+    font-style: italic;
+    color: #666;
+}
+
+#text {
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 10px;
+}
+
+#user-input {
+    width: 90%;
+    padding: 10px;
+    font-size: 16px;
+    margin-top: 10px;
+}
+
+#result {
+    font-size: 20px;
+    margin-top: 20px;
+    font-weight: bold;
+}
+
+#start-button {
+    background-color: #4CAF50;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 20px;
+}
+
+#start-button:hover {
+    background-color: #45a049;
+}
