b45d30e60a50be9bad481eaa35c186009a184a47
JTeo99 <>
2023-11-05 18:41:15 +0000

    script.js comments updated and directory paths edited

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -104,9 +104,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Function to start the game once.
+    /* 
+    Function to start the game once. Game disables the difficulty and start
+    buttons on game commencement and starts a timer to measure the time taken
+    to complete the typing.
+    */
     function startGameOnce() {
-        // Disable the difficulty and start buttons once the game is started.
         startButton.removeEventListener("click", startGameOnce);
         easyButton.disabled = true;
         mediumButton.disabled = true;
@@ -121,25 +124,25 @@ document.addEventListener("DOMContentLoaded", function () {
         userInput.value = "";
         userInput.addEventListener("input", checkInput);
         userInput.focus();
-        // Starting the countup to measure time for each game
         clearInterval(timerInterval);
         seconds = 0;
         updateTimer();
         timerInterval = setInterval(updateTimer, 1000);
     }
 
-    // Function to allow player to see the timer in 00:00 format
+    /* 
+    Function to allow player to see the timer in 00:00 format. Divides total
+    seconds by 60 and uses math floor to make a whole number for minutes. "%"
+    gets remainder from remaining seconds. Changes numbers into strings for
+    display and ensure that if minutes or seconds is less than 10, a 0 is placed
+    ahead
+    */
     function updateTimer() {
-        //divides total seconds by 60 and uses math floor to get whole number to get minutes
         const minutes = Math.floor(seconds / 60);
-        //uses % to get remainder from dividing seconds by 60 and displays as the seconds
         const remainingSeconds = seconds % 60;
-
-        //turns the numbers into strings that can be displayed and ensures that if minutes or seconds is <10 a 0 is put in front
         const minutesStr = String(minutes).padStart(2, '0');
         const secondsStr = String(remainingSeconds).padStart(2, '0');
         
-        //constructing the display in 00:00 format
         timerElement.textContent = `Time: ${minutesStr}:${secondsStr}`;
         seconds++;
     }
@@ -154,28 +157,28 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Function to check user input.
+    /*
+    Function to check user input. Fetches the quotes to type and value entered
+    by user and builds a string for typed text for display. A loop is created
+    that iterates each character of the quote to check whether the user input
+    matches the quote and flags the letters as either correct or not. Seen on
+    the game through CSS styling on the text box. If the typed quote matches the
+    given quote completely, game will show the next quote or end game if no new
+    quote is available for that difficulty.
+    */
     function checkInput() {
         if (currentQuoteIndex < sentencesToType.length) {
-            //Fetches the current quote to type
             const currentQuote = sentencesToType[currentQuoteIndex];
-            //Fetches the value enterd by the user input
             const userInputValue = userInput.value;
-            //creating empty string for building styled display of typed characters
             let typedText = '';
 
-            //creating a loop that iterates each character of the quote
-            //It checks whether the user input matches the quote and flags the letters as either correct or incorrect and applies css to the character
             for (let i = 0; i < currentQuote.length; i++) {
                 const isCorrect = currentQuote[i] === userInputValue[i];
                 const charClass = isCorrect ? 'correct' : (userInputValue[i] === ' ' ? 'space-mistyped' : (userInputValue[i] ? 'mistyped' : ''));
                 typedText += `<span class="${charClass}">${currentQuote[i]}</span>`;
             }
-            //updates the display with the styled characters
             textElement.innerHTML = typedText;
 
-            //checks whether the final input matches quote
-            //if correct then the game will show next quote otherwise end the game if there are no other quotes
             if (userInputValue === currentQuote) {
                 currentQuoteIndex++;
 
@@ -184,7 +187,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 } else {
                     endGame();
                 }
-
                 userInput.value = "";
             }
         }
@@ -193,50 +195,43 @@ document.addEventListener("DOMContentLoaded", function () {
     // Array to store game history.
     const gameHistory = [];
 
-    // Function to add a game to the history.
+    /*
+    Function to add a game to the history by using HTML elements to create
+    table. populates table with game related data and makes the table only as
+    long as it needs to be by adding rows as games are complete. Organises data
+    and appends to new rows. Makes sure that the maximum history length is 10
+    and removes older history.
+    */
     function addGameToHistory(difficulty, timeSpent, wordsPerMinute) {
-        //referencing the HTML elements
         const tableBody = document.querySelector("#game-history tbody");
         const newRow = document.createElement("tr");
-        //references for tables to populate with the game related data
         const gameCell = document.createElement("td");
         const difficultyCell = document.createElement("td");
         const timeSpentCell = document.createElement("td");
         const wordsPerMinuteCell = document.createElement("td");
 
-        //populating the game related data
-        //increasing the game number by 1 each time game is completed
         gameCell.textContent = gameHistory.length + 1;
-        //difficulty level the game was completed at
         difficultyCell.textContent = difficulty;
 
-        //time spent on the game
         const minutes = Math.floor(timeSpent / 60);
         const seconds = timeSpent % 60;
         const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
         timeSpentCell.textContent = formattedTime;
 
-        //words per minute (WPM)
         wordsPerMinuteCell.textContent = wordsPerMinute;
 
-        //append each cell to the new row
         newRow.appendChild(gameCell);
         newRow.appendChild(difficultyCell);
         newRow.appendChild(timeSpentCell);
         newRow.appendChild(wordsPerMinuteCell);
 
-        // Append the new row to the game history table.
         tableBody.appendChild(newRow);
 
-        // Append the new row to the game history table.
         if (gameHistory.length >= 10) {
-            // If so, remove the oldest entry (the first row) from the table.
             tableBody.removeChild(tableBody.firstElementChild);
-            // Also, remove the oldest entry from the game history data array.
             gameHistory.shift();
         }
 
-        // Add the current game's details (difficulty, time spent, and WPM) to the game history data array.
         gameHistory.push({
             difficulty,
             timeSpent,
@@ -244,30 +239,27 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Function to end the game.
+    /*
+    Function to end the game. Stops the timer by clearing the update timer
+    interval and uses time to calculate wpm to 2d.p.
+    Once game has finished the start button changes to a restart game button and
+    the button is enabled again. disables "input" event listener so it no longer
+    checks for input. Re-enables difficulty questions.
+    */
     function endGame() {
-        //stops the timer by clearing interval used to update timer
         clearInterval(timerInterval);
-        //calculates the WPM, creates result to 2dp
         const wordsPerMinute = (
             (sentencesToType.join(" ").split(" ").length / (seconds / 60))
         ).toFixed(2);
         resultElement.textContent = `Game Over! Your typing speed (${selectedDifficulty}): ${wordsPerMinute} WPM`;
-        //changes content of the start game button to restart game
         startButton.textContent = "Restart Game";
-        //renables the start game button
         startButton.disabled = false;
-        //removes the even listener for the input so that it no longer checks after game ends
         userInput.removeEventListener("input", checkInput);
-        //indicates the game is ended
         gameStarted = false;
-        //clears the highlight on difficulty buttons
         highlightDifficultyButton("");
-        //clears the game related text
         quoteElement.textContent = "";
         textElement.textContent = "";
 
-        //re-enables difficulty buttons
         addGameToHistory(selectedDifficulty, seconds, wordsPerMinute);
         easyButton.disabled = false;
         mediumButton.disabled = false;
